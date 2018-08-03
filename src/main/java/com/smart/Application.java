1d35e4ecf56954efbaaf884421d7fcd296edd34d
niuhaijun <niuhaijun@100tal.com>
2018-08-03 13:15:06 +0800

    init repository

---
+++ src/main/java/com/smart/Application.java
@@ -0,0 +1,33 @@
+package com.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class Application extends SpringBootServletInitializer implements WebApplicationInitializer {
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public PlatformTransactionManager txManager(DataSource dataSource) {
+    return new DataSourceTransactionManager(dataSource);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Application.class);
+  }
+}
+
